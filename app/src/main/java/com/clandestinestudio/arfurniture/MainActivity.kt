5b68bcd273b146488ff49d722e17081e3984b9fc
Aswin Karki <aswin.karki44@gmail.com>
2023-01-19 00:05:39 +0545

    some bug fixes

--- app/src/main/java/com/clandestinestudio/arfurniture/MainActivity.kt
+++ app/src/main/java/com/clandestinestudio/arfurniture/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 //        startActivity(Intent(this, UnityHandlerActivity::class.java))
+        supportActionBar?.title = "Home"
     }
 }
