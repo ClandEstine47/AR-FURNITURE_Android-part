3d95d43ad92191cafb12a67aef2db7ad99d25de9
Aswin Karki <aswin.karki44@gmail.com>
2023-01-22 20:39:41 +0545

    added favorite button and its animation in details page

--- app/src/main/java/com/clandestinestudio/arfurniture/DetailsActivity.kt
+++ app/src/main/java/com/clandestinestudio/arfurniture/DetailsActivity.kt
@@ -2,24 +2,31 @@ package com.clandestinestudio.arfurniture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.models.SlideModel
 import com.squareup.picasso.Picasso
+import kotlin.concurrent.timer
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        var isFavorite: Boolean = false
+
         val furnitureNameView: TextView = findViewById(R.id.tv_name_detailsPage)
-        val furnitureCategoryView : TextView = findViewById(R.id.tv_category_detailsPage)
+        val furnitureCategoryView: TextView = findViewById(R.id.tv_category_detailsPage)
 //        val furnitureImageView : ImageView = findViewById(R.id.iv_detailsPage)
-        val furnitureDescriptionView : TextView = findViewById(R.id.tv_description_detailsPage)
+        val furnitureDescriptionView: TextView = findViewById(R.id.tv_description_detailsPage)
         val imageSlider: ImageSlider = findViewById(R.id.image_slider)
 
-        val bundle: Bundle?= intent.extras
+        val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottieFavorite)
+
+        val bundle: Bundle? = intent.extras
         val furnitureName = bundle!!.getString("name")
         val furnitureCategory = bundle!!.getString("category")
         val furnitureImageUrl = bundle!!.getString("image_url")
@@ -36,5 +43,19 @@ class DetailsActivity : AppCompatActivity() {
         furnitureDescriptionView.text = furnitureDescription
 //        Picasso.get().load(furnitureImageUrl).into(furnitureImageView)
         imageSlider.setImageList(imageList)
+
+        lottieAnimationView.setOnClickListener {
+            if (isFavorite) {
+                lottieAnimationView.setMinAndMaxProgress(0.5f, 1.0f)
+                lottieAnimationView.playAnimation()
+                isFavorite = false
+
+            } else {
+                lottieAnimationView.setMinAndMaxProgress(0.0f, 0.5f)
+                lottieAnimationView.playAnimation()
+                isFavorite = true
+            }
+
+        }
     }
 }
