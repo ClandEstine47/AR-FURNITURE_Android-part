33504ce4912c8700c6f860ed02e55a2248d315d0
Aswin Karki <aswin.karki44@gmail.com>
2023-01-22 19:24:01 +0545

    added imageslider for details page to show dimension image as well as furniture image

--- app/src/main/java/com/clandestinestudio/arfurniture/DetailsActivity.kt
+++ app/src/main/java/com/clandestinestudio/arfurniture/DetailsActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
 import com.squareup.picasso.Picasso
 
 class DetailsActivity : AppCompatActivity() {
@@ -13,8 +15,9 @@ class DetailsActivity : AppCompatActivity() {
 
         val furnitureNameView: TextView = findViewById(R.id.tv_name_detailsPage)
         val furnitureCategoryView : TextView = findViewById(R.id.tv_category_detailsPage)
-        val furnitureImageView : ImageView = findViewById(R.id.iv_detailsPage)
+//        val furnitureImageView : ImageView = findViewById(R.id.iv_detailsPage)
         val furnitureDescriptionView : TextView = findViewById(R.id.tv_description_detailsPage)
+        val imageSlider: ImageSlider = findViewById(R.id.image_slider)
 
         val bundle: Bundle?= intent.extras
         val furnitureName = bundle!!.getString("name")
@@ -23,9 +26,15 @@ class DetailsActivity : AppCompatActivity() {
         val furnitureDescription = bundle!!.getString("description")
         val furnitureDimensionImgUrl = bundle!!.getString("dimensions")
 
+        val imageList = ArrayList<SlideModel>()
+
+        imageList.add(SlideModel(furnitureImageUrl))
+        imageList.add(SlideModel(furnitureDimensionImgUrl))
+
         furnitureNameView.text = furnitureName
         furnitureCategoryView.text = furnitureCategory
         furnitureDescriptionView.text = furnitureDescription
-        Picasso.get().load(furnitureImageUrl).into(furnitureImageView)
+//        Picasso.get().load(furnitureImageUrl).into(furnitureImageView)
+        imageSlider.setImageList(imageList)
     }
 }
