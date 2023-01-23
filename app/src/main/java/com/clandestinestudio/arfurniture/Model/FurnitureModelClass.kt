635db76d8773bdef0cbaa0c8ee9d0e1b20a7c188
Aswin Karki <aswin.karki44@gmail.com>
2023-01-23 15:02:01 +0545

    implemented parceable in Furniture model class to be able to send the entire list to the next activity

--- app/src/main/java/com/clandestinestudio/arfurniture/Model/FurnitureModelClass.kt
+++ app/src/main/java/com/clandestinestudio/arfurniture/Model/FurnitureModelClass.kt
@@ -1,10 +1,45 @@
 package com.clandestinestudio.arfurniture.Model
+import android.os.Parcel
+import android.os.Parcelable
 
 class FurnitureModelClass (
-    val id: String,
-    val name: String,
-    val category: String,
-    val image_url: String,
-    val dimensions: String,
-    val description: String,
-)
+    val id: String?,
+    val name: String?,
+    val category: String?,
+    val image_url: String?,
+    val dimensions: String?,
+    val description: String?,
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(category)
+        parcel.writeString(image_url)
+        parcel.writeString(dimensions)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<FurnitureModelClass> {
+        override fun createFromParcel(parcel: Parcel): FurnitureModelClass {
+            return FurnitureModelClass(parcel)
+        }
+
+        override fun newArray(size: Int): Array<FurnitureModelClass?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
