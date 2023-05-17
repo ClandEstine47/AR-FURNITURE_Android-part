0bd35de74d86ea0e33e34ca0ebf35e0aaa6724b6
Aswin Karki <Aswin.karki44@gmail.com>
2023-05-17 18:26:06 +0545

    added gradient in app name of splashscreen

--- app/src/main/java/com/clandestinestudio/arfurniture/SplashScreen.kt
+++ app/src/main/java/com/clandestinestudio/arfurniture/SplashScreen.kt
@@ -8,14 +8,28 @@ import com.airbnb.lottie.Lottie
 import com.airbnb.lottie.LottieAnimationView
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import kotlinx.coroutines.*
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.Color
 
 class SplashScreen : AppCompatActivity() {
-
+    private lateinit var gradientTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
+
+        gradientTextView = findViewById(R.id.appname)
+        val startColor = Color.parseColor("#4056A1")
+        val endColor = Color.parseColor("#FF6830")
+
+        val gradient = LinearGradient(
+            0f, 0f, gradientTextView.paint.measureText(gradientTextView.text.toString()), 0f,
+            startColor, endColor, Shader.TileMode.CLAMP
+        )
+
+        gradientTextView.paint.shader = gradient
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(6000L)
